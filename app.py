3232baa0a087e6b23c5df7db86901f23ed6be896
Angiemou06 <59827584+Angiemou06@users.noreply.github.com>
2023-01-23 18:39:31 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-# 載入LineBot所需要的模組
+# 載入LineBot所需要的套件
 import os
 from flask import Flask, request, abort
 
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import *
 
 app = Flask(__name__)
@@ -11,16 +15,14 @@ app = Flask(__name__)
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi(
     'fmF/h5WT/GZn8xZ4xf9T6N+atF2mip3C/fQU93pT2QxwUXQLvD7eGGfRDpP9tS82F3biMugx31TW2t323PN2a8w1FxMJmMAnEH7oeQJwWhB7KG+1zdzBZRhBxfM6fFtalTMLgYiVUGb4nbNoWpXlSgdB04t89/1O/w1cDnyilFU=')
-
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('0f9b9e33b2458ce63155dbf0b38b2542')
 
 line_bot_api.push_message(
     'U58c4749f4bb186024390fc2a8dcfe5df', TextSendMessage(text='你可以開始了'))
 
+
 # 監聽所有來自 /callback 的 Post Request
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -38,16 +40,17 @@ def callback():
 
     return 'OK'
 
-    # 訊息傳遞區塊
+
+# 訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-    # 主程式
+
+# 主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+Copy
